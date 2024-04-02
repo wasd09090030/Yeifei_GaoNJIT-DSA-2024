f560d7e7af6bef0ef7bc09583b83600ed7c876a9
qq862 <qq86280630qq@163.com>
2024-04-02 18:04:21 +0800

    commit queue

--- 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
+++ 02-2-queue/src/main/java/oy/tol/tra/QueueImplementation.java
@@ -1,13 +1,13 @@
 package oy.tol.tra;
 
-import java.util.Queue;
+
 
 
 public class QueueImplementation <E> implements QueueInterface<E> {
     private Object[] itemArray;
-   private int capacity;
-   private int front = -1;
-   private int rear = -1;
+    private int capacity;
+    private int front = -1;
+    private int rear = -1;
 
 
     public  QueueImplementation(int capacity) throws QueueAllocationException {
@@ -30,60 +30,52 @@ public class QueueImplementation <E> implements QueueInterface<E> {
     }
 
 
-
     @Override
-public int capacity() {
-    return this.capacity;
-    
-}
-@Override
-public void enqueue(E element) throws QueueAllocationException, NullPointerException {
+    public int capacity() {
+        return this.capacity;
 
-    if (element == null)
-    throw new NullPointerException();
-    else if (this.rear + 1 == this.capacity)
-         {this.IncreaseCapacity();
-         this.itemArray[++this.rear] = element;}
-      else {
-         this.itemArray[++this.rear] = element;
-      }
-    
-}
+    }
+    @Override
+    public void enqueue(E element) throws QueueAllocationException, NullPointerException {
 
-public void IncreaseCapacity() {
-    this.capacity = this.capacity * 2;
-    Object[] itemArray01;
-    itemArray01 = new Object[this.capacity];
+        if (element == null)
+            throw new NullPointerException();
+        else if (this.rear + 1 == this.capacity)
+        {this.increaseCapacity();
+            this.itemArray[++this.rear] = element;}
+        else {
+            this.itemArray[++this.rear] = element;
+        }
 
+    }
 
-    for(int i=0;i<this.itemArray.length;i++){
-          itemArray01[i] = this.itemArray[i];
-      }
-    this.itemArray=itemArray01;
+    public void increaseCapacity() {
+        this.capacity = this.capacity * 2;
+        Object[] itemArray01;
+        itemArray01 = new Object[this.capacity];
+        int t=0;
 
 
- }
 
+        for(int i = 0; i < this.itemArray.length; i++){
+            if(this.itemArray[i]!=null){
 
-    public void reSetArray(){
+                itemArray01[t] = this.itemArray[i];
+                t++;
 
-        int k=0;
-        Object[] itemArray02 = new Object[this.capacity];
-
-
-        for(int i = this.front + 1; i <= this.rear; i++){
-
-            itemArray02[k] = this.itemArray[i];
-            k++;
+            }
 
         }
-        this.itemArray=itemArray02;
-
+        this.itemArray=itemArray01;
         this.front=-1;
-        this.rear=this.rear-1;
+        this.rear=t-1;
 
 
     }
+
+
+
+
 
 
 
@@ -94,53 +86,54 @@ public void IncreaseCapacity() {
         else
             this.front++;
         E element01=(E) this.itemArray[this.front];
+        this.itemArray[this.front]=null;
 
-        this.reSetArray();
+
 
 
         return element01;
 
     }
-@Override
-public E element() throws QueueIsEmptyException {
-    if (this.isEmpty())
-    throw new QueueIsEmptyException("QueueIsEmpty");
-    else
-    return (E) this.itemArray[this.front+1];
-   
-    
-}
-@Override
-public int size() {
-    
-    return this.rear-this.front;
-    
-}
-@Override
-public boolean isEmpty() {
-   return  this.rear == this.front;
-  
-}
-@Override
-public void clear() {
-   
-    this.front=this.rear=-1;
-   
-}
+    @Override
+    public E element() throws QueueIsEmptyException {
+        if (this.isEmpty())
+            throw new QueueIsEmptyException("QueueIsEmpty");
+        else
+            return (E) this.itemArray[this.front+1];
 
-   
-@Override
-public String toString() {
-   StringBuilder builder = new StringBuilder("[");
-   for (var index = this.front+1; index <= this.rear; index++) {
-      builder.append(this.itemArray[index].toString());
-      if (index < this.rear) {
-         builder.append(", ");
-      }
-   }
-   builder.append("]");
-   return builder.toString();
-}
+
+    }
+    @Override
+    public int size() {
+
+        return this.rear-this.front;
+
+    }
+    @Override
+    public boolean isEmpty() {
+        return  this.rear == this.front;
+
+    }
+    @Override
+    public void clear() {
+
+        this.front=this.rear=-1;
+
+    }
+
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("[");
+        for (var index = this.front+1; index <= this.rear; index++) {
+            builder.append(this.itemArray[index].toString());
+            if (index < this.rear) {
+                builder.append(", ");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
 
 
 
